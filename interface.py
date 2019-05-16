236d228a747f83bafc62d4810fd2bc114a293ec6
jmseguraf01 <jmseguraf01@elpuig.xeill.net>
2019-05-16 10:40:50 +0200

    Cambios: creada interfaz de support y titulos en esta, acabada la funcion de cambiar el color del tema y situar la interfaz en medio de la pantalla.

--- interface.py
+++ interface.py
@@ -2,26 +2,32 @@
 #-*- coding: utf-8 -*-
 
 # © Juan Miguel Segura Fernandez and Alejandro Mallen Gomez
-# This is Free Software. License GNU GPLv3
+# This is Free Software. License GNU GPLv3.
 
 from Tkinter import *
 from PIL import Image, ImageTk
 import webbrowser
 
 # Interface and title interface
-#--------------------------------------------#
 interface = Tk()
-interface.title("ClamTK")
-interface.geometry("850x600")
+interface.title("ClamPy")
 interface.config(background="#2A0A1B")
+interface.resizable(0,0)
+
+# Interface in the middle of the screeen
+windowWidth = interface.winfo_reqwidth()
+windowHeight = interface.winfo_reqheight()
+positionRight = int(interface.winfo_screenwidth()/2 - windowWidth)
+positionDown = int(interface.winfo_screenheight()/2 - windowHeight)
+interface.geometry("850x600+%s+%s"%(positionRight,positionDown))
+
 title = Label(interface,
-    text="ClamTK",
+    text="ClamPy",
     foreground="#FF0000",
     font="Helveltica 20",
     background="#2A0A1B",)
 title.pack()
 title.place(x=78, y=15)
-#----------------------------------------------#
 
 # Top logo and text
 #----------------------------------------------#
@@ -31,11 +37,9 @@ logo_image = Label(interface,
     background="#2A0A1B",)
 logo_image.pack()
 logo_image.place(x=3, y=3)
-#---------------------------------------------#
 
 # Left top menu
 #---------------------------------------------#
-
 # Web Page
 # Web page function
 def open_web_page(event):
@@ -60,23 +64,63 @@ def open_git_hub(event):
     webbrowser.open_new("https://github.com/Cisco-Talos/clamav-devel")
 
 # Github link top label
-label_git_hub = Label(interface,
+label_github = Label(interface,
     text="GitHub",
     background="#2A0A1B",
     foreground="white",
     cursor="hand2",
     font="Helvetica 12 italic",
     )
-label_git_hub.pack()
-label_git_hub.place(x=550, y=20)
+label_github.pack()
+label_github.place(x=550, y=20)
 # Calls open_git_hub function in the label
-label_git_hub.bind("<Button-1>", open_git_hub)
+label_github.bind("<Button-1>", open_git_hub)
 
-# Support
+# ---------------------------------------
+#          Support
 # Support link function and Support interface
 def open_support(event):
+    interface.destroy()
+    # Support interface
     interface_support = Tk()
-    interface_support.pack()
+    interface_support.title("ClamPy")
+    interface_support.config(bg="#2A0A1B")
+    interface_support.resizable(0,0)
+
+    # Interface in the middle of the screeen
+    windowWidth = interface_support.winfo_reqwidth()
+    windowHeight = interface_support.winfo_reqheight()
+    positionRight = int(interface_support.winfo_screenwidth()/2 - windowWidth)
+    positionDown = int(interface_support.winfo_screenheight()/2 - windowHeight/2)
+    interface_support.geometry("850x400+%s+%s"%(positionRight,positionDown))
+
+
+    # Image and label logo interface
+    logo_interface_support=ImageTk.PhotoImage(file="img/logo.png")
+    label_logo_interface_support=Label(interface_support, image=logo_interface_support, bg="#2A0A1B")
+    label_logo_interface_support.pack()
+    label_logo_interface_support.place(x=3, y=3)
+
+    # Label license
+    license_interface_support=Label(interface_support, text="License", bg="#2A0A1B", fg="#FF0000", font="Helveltica 15 italic", cursor="hand2"    )
+    license_interface_support.pack()
+    license_interface_support.place(x=100, y=25)
+
+    # Label program
+    program_interface_support=Label(interface_support, text="Program", bg="#2A0A1B", fg="#FF0000", font="Helveltica 15 italic", cursor="hand2"    )
+    program_interface_support.pack()
+    program_interface_support.place(x=200, y=25)
+
+    # Label contact
+    contact_interface_support=Label(interface_support, text="Contact", bg="#2A0A1B", fg="#FF0000", font="Helveltica 15 italic", cursor="hand2"    )
+    contact_interface_support.pack()
+    contact_interface_support.place(x=300, y=25)
+
+
+    interface_support.mainloop()
+
+# Finish support
+# ------------------------------------------
 
 # Support link top label
 label_support = Label(interface,
@@ -93,17 +137,41 @@ label_support.bind("<Button-1>", open_support)
 
 # Themes
 # Theme function changes dark or light
-def change_theme(event):
-    color=textvariable.get()
-    print color
+def theme_white(event):
+    # Change interface, label, and buttons colors
+    interface.config(bg="#d6d6c2")
+    logo_image.config(bg="#d6d6c2")
+    title.config(bg="#d6d6c2")
+    label_web.config(bg="#d6d6c2", fg="black")
+    label_github.config(bg="#d6d6c2", fg="black")
+    label_support.config(bg="#d6d6c2", fg="black")
+    label_theme.config(bg="#d6d6c2", fg="black")
 
-#--------------------------------------------#
+def theme_purpule(event):
+    # Change interface, label, and buttons colors
+    interface.config(bg="#2A0A1B")
+    logo_image.config(bg="#2A0A1B")
+    title.config(bg="#2A0A1B")
+    label_web.config(bg="#2A0A1B", fg="white")
+    label_github.config(bg="#2A0A1B", fg="white")
+    label_support.config(bg="#2A0A1B", fg="white")
+    label_theme.config(bg="#2A0A1B", fg="white")
+
+def theme_black(event):
+    # Change interface, label, and buttons colors
+    interface.config(bg="#262626")
+    logo_image.config(bg="#262626")
+    title.config(bg="#262626")
+    label_web.config(bg="#262626", fg="white")
+    label_github.config(bg="#262626", fg="white")
+    label_support.config(bg="#262626", fg="white")
+    label_theme.config(bg="#262626", fg="white")
+
 # Label theme
 label_theme=Label(interface,
     text="Theme:",
     background="#2A0A1B",
     foreground="white",
-    cursor="hand2",
     font="Helvetica 12 italic"
     )
 label_theme.pack()
@@ -111,27 +179,24 @@ label_theme.place(x=710, y=20)
 
 # Label of colors
 # ----------------
-v=StringVar()
-# White color image and label
-white_color=ImageTk.PhotoImage(file="img/color-blanco.png")
-label_white_color=Label(interface, image=white_color, cursor="hand2", textvariable=v("white"))
-label_white_color.pack()
-label_white_color.place(x=785, y=25)
-label_white_color.bind("<Button-1>", change_theme)
 
-# Purpule color image and label
-purpule_color=ImageTk.PhotoImage(file="img/color-morado.png")
-label_purpule_color=Label(interface, image=purpule_color, cursor="hand2", textvariable="purpule")
-label_purpule_color.pack()
-label_purpule_color.place(x=800, y=25)
-label_purpule_color.bind("<Button-1>", change_theme)
+# White color canvas
+canvas_white = Canvas(interface, bg="#d6d6c2", highlightbackground="white", height=10, width=10, cursor="hand2")
+canvas_white.pack()
+canvas_white.place(x=780, y=30)
+canvas_white.bind("<Button-1>", theme_white)
 
-# Black color image and label
-black_color=ImageTk.PhotoImage(file="img/color-negro.png")
-label_black_color=Label(interface, image=black_color, cursor="hand2", textvariable="black")
-label_black_color.pack()
-label_black_color.place(x=815, y=25)
-label_black_color.bind("<Button-1>", change_theme)
+# Purpule color canvas
+canvas_purpule = Canvas(interface, bg="#2A0A1B", highlightbackground="white",height=10, width=10, cursor="hand2")
+canvas_purpule.pack()
+canvas_purpule.place(x=795, y=30)
+canvas_purpule.bind("<Button-1>", theme_purpule)
+
+# Black color canvas
+canvas_black = Canvas(interface, bg="#262626", highlightbackground="white",height=10, width=10, cursor="hand2")
+canvas_black.pack()
+canvas_black.place(x=810, y=30)
+canvas_black.bind("<Button-1>", theme_black)
 
 
 interface.mainloop()
